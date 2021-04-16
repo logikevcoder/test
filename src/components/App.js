dc8581ead348b385b518f0a62cf9c0d01bee5583
Kevin Napier <kevin.napier@rotary.org>
2021-04-16 18:10:27 -0500

    Converted App to a class based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+import React, { Component } from 'react';
 import '../styles/App.scss';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  render() {
+    return <div className='App'></div>;
+  }
 }
 
 export default App;
