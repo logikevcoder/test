4b4068003f5b4ae449fac4f0b7880891d812a5dc
Kevin Napier <kevin.napier@rotary.org>
2021-04-17 13:08:05 -0500

    Imported the AutoComplete component, built the getData function to handle API data, built updateField function to handle input text updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 import '../styles/App.scss';
+import Autocomplete from './Autocomplete';
 
 class App extends Component {
+  state = {
+    update: false,
+    keyword: '',
+    results: [],
+  };
+
+  getData = async (inputText) => {
+    let api_url, data;
+    if (inputText !== '') {
+      try {
+        api_url = await fetch(
+          `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${inputText}&apikey=GUO9GOF82DVL1REY`
+        );
+        data = await api_url.json();
+        console.log(data);
+        this.setState({ results: data.bestMatches });
+
+        return data;
+      } catch (err) {
+        console.log(err.message);
+      }
+    } else this.setState({ results: [] });
+  };
+
+  updateField = (field, value, update = true) => {
+    if (update) this.getData(value);
+    this.setState({ [field]: value });
+  };
+
   render() {
-    return <div className='App'></div>;
+    const { results, keyword } = this.state;
+
+    return (
+      <div className='App'>
+        <Autocomplete
+          placeholder={'Search for a stock'}
+          results={results}
+          keyword={keyword}
+          updateField={this.updateField}
+        />
+      </div>
+    );
   }
 }
 
